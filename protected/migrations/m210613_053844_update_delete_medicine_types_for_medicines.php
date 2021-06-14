73002648f6ae8050cbd74b27f37bb61265c4e7f4
root <alpeshspce20@gmail.com>
2021-06-14 13:30:11 +0530

    OPD-6v3

---
+++ protected/migrations/m210613_053844_update_delete_medicine_types_for_medicines.php
@@ -0,0 +1,24 @@
+<?php
+
+class m210613_053844_update_delete_medicine_types_for_medicines extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
